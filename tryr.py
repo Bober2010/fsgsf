e971bd8326fe2796ebe48867fd295763a54de09b
Bober2010 <115387919+Bober2010@users.noreply.github.com>
2024-03-13 17:54:55 +0700

    Add files via upload

---
+++ tryr.py
@@ -0,0 +1,10 @@
+f = open("jh.txt","r",encoding="utf-8")
+d = 0
+a = 0
+l = 0
+for i in f:
+    l += 1
+    d += int(i)
+    (d / l)
+    a = d / l
+print(a) 
---
+++ овф.py
@@ -0,0 +1,4 @@
+f = open("jh.txt","r",encoding = "utf - 8")
+
+for l in f:
+    print(l)
